cf23d1628358b0dd05c9b542bee6203d6e40e024
kamilFitrzyk <kamilfitrzyk89@gmail.com>
2022-04-07 20:54:51 +0200

    read from file

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-import ReadFromFile
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    data = open("score_L2.lvm", "r")
-    for x in data:
-        print(x)
+    file = open("score_L2.lvm", "r")
+
+    data = [], []
+
+    for line in file:
+        line = line.replace(",",".")
+        size = len(line)
+        numbers = line[:size - 1]
+        numbers = numbers.split("	")
+
+        data[0].append(float(numbers[0]))
+        data[1].append(float(numbers[1]))
+
+
